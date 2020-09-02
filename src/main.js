1358dd4604805bbba90fbfa632dea7d0e8765792
snow <snowsuika@gmail.com>
2020-09-02 12:41:56 +0800

    <add> 新增 VueFormWizard 套件

--- src/main.js
+++ src/main.js
@@ -2,12 +2,16 @@ import Vue from 'vue';
 // import axios from 'axios';
 // import VueAxios from 'vue-axios';
 import jquery from 'jquery';
-
 import 'bootstrap';
+
+import VueFormWizard from 'vue-form-wizard';
+import 'vue-form-wizard/dist/vue-form-wizard.min.css';
+
 import App from './App.vue';
 import router from './router';
 
 window.$ = jquery;
+Vue.use(VueFormWizard);
 
 Vue.config.productionTip = false;
 // Vue.use(VueAxios, axios);
