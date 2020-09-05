ce9d4c418e87ac75e77c32d7731f062770f4b088
snow <snowsuika@gmail.com>
2020-09-05 20:30:43 +0800

    <add> 家屬端我的訂單、服務對象、我的帳號 UI

---
+++ src/views/backendMember/MemberDashboard.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="about">
+    <navbar></navbar>
+    <main>
+      <div
+        class="banner d-flex justify-content-center align-items-center bg-cover text-white"
+        style="height:250px;"
+      ></div>
+      <!-- 照服員 -->
+      <div class="container py-5" style="margin-top: -120px">
+        <div class="row">
+          <div class="col-12 col-lg-2">
+            <div class="bg-white py-3 shadow-sm radius-2 mb-3 mb-lg-0">
+              <sidebar></sidebar>
+            </div>
+          </div>
+          <div class="col-12 col-lg-10 mt-3 mt-md-0">
+            <router-view></router-view>
+          </div>
+        </div>
+      </div>
+    </main>
+
+    <footer-bar></footer-bar>
+    <!-- <modal-register-login :action="navModalAction"></modal-register-login> -->
+  </div>
+</template>
+<script>
+import Navbar from '@/components/layout/Navbar.vue';
+import FooterBar from '@/components/layout/Footer.vue';
+import Sidebar from '@/components/layout/Sidebar.vue';
+// import ModalRegisterLogin from '@/components/ModalRegisterLogin.vue';
+
+export default {
+  name: 'MemberDashboard',
+  data() {
+    return {
+      token: '',
+      userId: '',
+      userMail: '',
+      checkSuccess: false,
+      navModalAction: ''
+    };
+  },
+  components: {
+    Navbar,
+    Sidebar,
+    FooterBar
+    // ModalRegisterLogin
+  },
+  created() {
+    this.token = localStorage.getItem('token');
+    this.userId = localStorage.getItem('userId');
+    this.userMail = localStorage.getItem('userMail');
+
+    if (!this.token) {
+      this.$router.push('/');
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+body {
+  background-color: #f6f3ee;
+}
+.banner {
+  background-color: gray;
+  background-position: center;
+  background-size: cover;
+}
+
+table.table-radius {
+  border: 1px solid #c3c3c3;
+  border-radius: 10px;
+
+  border-spacing: 0px;
+  border-collapse: separate;
+}
+
+table.table-radius th:first-child {
+  border-top-left-radius: 10px;
+  border-bottom-left-radius: 10px;
+}
+
+table.table-radius th:last-child {
+  border-top-right-radius: 10px;
+  border-bottom-right-radius: 10px;
+}
+
+table.table-radius th,
+table.table-radius td {
+  vertical-align: middle;
+}
+</style>
---
+++ src/views/backendMember/MemberDashboardOrder.vue
@@ -0,0 +1,493 @@
+<template>
+  <div>
+    <div class="card shadow-sm">
+      <div class="card-header">
+        <ul class="nav nav-tabs card-header-tabs" id="nav-tab" role="tablist">
+          <li class="nav-item">
+            <a
+              class="nav-link active"
+              id="nav-orderWaitconfirm-tab"
+              data-toggle="tab"
+              href="#orderWaitconfirm"
+              role="tab"
+              aria-controls="nav-home"
+              aria-selected="true"
+              >待照服員確認中
+              <span class="badge badge-pill badge-secondary text-white"
+                >2</span
+              ></a
+            >
+          </li>
+          <li class="nav-item">
+            <a
+              class="nav-link"
+              id="nav-orderProcess-tab"
+              data-toggle="tab"
+              href="#orderProcess"
+              role="tab"
+              aria-controls="nav-home"
+              aria-selected="true"
+              >訂單處理中
+              <span class="badge badge-pill badge-secondary text-white"
+                >2</span
+              ></a
+            >
+          </li>
+          <li class="nav-item">
+            <a
+              class="nav-link"
+              id="nav-orderService-tab"
+              data-toggle="tab"
+              href="#orderService"
+              role="tab"
+              aria-controls="nav-home"
+              aria-selected="true"
+              >服務進行中
+              <span class="badge badge-pill badge-secondary text-white"
+                >2</span
+              ></a
+            >
+          </li>
+          <li class="nav-item">
+            <a
+              class="nav-link"
+              id="nav-orderEvaluate-tab"
+              data-toggle="tab"
+              href="#orderEvaluate"
+              role="tab"
+              aria-controls="nav-home"
+              aria-selected="true"
+              >待評價中
+              <span class="badge badge-pill badge-secondary text-white"
+                >2</span
+              ></a
+            >
+          </li>
+          <li class="nav-item">
+            <a
+              class="nav-link"
+              id="nav-orderFinish-tab"
+              data-toggle="tab"
+              href="#orderFinish"
+              role="tab"
+              aria-controls="nav-home"
+              aria-selected="true"
+              >已完成</a
+            >
+          </li>
+        </ul>
+      </div>
+      <div class="card-body">
+        <div class="tab-content" id="nav-tabContent">
+          <div
+            class="tab-pane fade show active"
+            id="orderWaitconfirm"
+            role="tabpanel"
+            aria-labelledby="nav-home-tab"
+          >
+            <div class="table-responsive">
+              <table class="table table-radius">
+                <tr class="table-light">
+                  <th scope="col">被服務對象</th>
+                  <th scope="col">照服人員</th>
+                  <th scope="col">服務時段</th>
+                  <th scope="col">訂單金額</th>
+                  <th scope="col">查看訂單詳細</th>
+                  <th scope="col">取消訂單</th>
+                </tr>
+
+                <tbody>
+                  <tr>
+                    <td class="text-center">王伯伯</td>
+                    <td>張照服</td>
+                    <td>
+                      <p>
+                        2020-08-15 <br />
+                        2020-08-16
+                      </p>
+                    </td>
+                    <td>4,000</td>
+                    <td>
+                      <button
+                        type="button"
+                        class="btn btn-primary-soft text-primary"
+                        data-toggle="modal"
+                        data-target="#orderDetail"
+                      >
+                        訂單細節
+                      </button>
+                    </td>
+                    <td>
+                      <button type="button" class="btn btn-primary">
+                        取消訂單
+                      </button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <div
+            class="tab-pane fade"
+            id="orderProcess"
+            role="tabpanel"
+            aria-labelledby="nav-profile-tab"
+          >
+            <div class="table-responsive">
+              <table class="table table-radius">
+                <tr class="table-light">
+                  <th scope="col">被服務對象</th>
+                  <th scope="col">照服人員</th>
+                  <th scope="col">服務時段</th>
+                  <th scope="col">訂單金額</th>
+                  <th scope="col">訂單狀態</th>
+                  <th scope="col">可執行動作</th>
+                  <th scope="col">查看訂單詳細</th>
+                </tr>
+
+                <tbody>
+                  <tr>
+                    <td class="text-center">王伯伯</td>
+                    <td>張照服</td>
+                    <td>
+                      <p>
+                        2020-08-15 <br />
+                        2020-08-16
+                      </p>
+                    </td>
+                    <td>4,000</td>
+                    <td>未付款</td>
+                    <td>
+                      <button type="button" class="btn btn-primary">
+                        付款
+                      </button>
+                    </td>
+                    <td>
+                      <button
+                        type="button"
+                        class="btn btn-primary-soft text-primary"
+                        data-toggle="modal"
+                        data-target="#orderDetail"
+                      >
+                        訂單細節
+                      </button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="text-center">王伯伯</td>
+                    <td>張照服</td>
+                    <td>
+                      <p>
+                        2020-08-15 <br />
+                        2020-08-16
+                      </p>
+                    </td>
+                    <td>4,000</td>
+                    <td>已付款</td>
+                    <td>
+                      <button type="button" class="btn btn-primary">
+                        取消訂單
+                      </button>
+                    </td>
+                    <td>
+                      <button
+                        type="button"
+                        class="btn btn-primary-soft text-primary"
+                        data-toggle="modal"
+                        data-target="#orderDetail"
+                      >
+                        訂單細節
+                      </button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <div
+            class="tab-pane fade"
+            id="orderService"
+            role="tabpanel"
+            aria-labelledby="nav-contact-tab"
+          >
+            <div class="table-responsive">
+              <table class="table table-radius">
+                <tr class="table-light">
+                  <th scope="col">被服務對象</th>
+                  <th scope="col">照服人員</th>
+                  <th scope="col">服務時段</th>
+                  <th scope="col">訂單金額</th>
+                  <th scope="col">查看照護紀錄</th>
+                  <th scope="col">查看訂單詳細</th>
+                  <th scope="col">終止契約</th>
+                </tr>
+
+                <tbody>
+                  <tr>
+                    <td class="text-center">王伯伯</td>
+                    <td>張照服</td>
+                    <td>
+                      <p>
+                        2020-08-15 <br />
+                        2020-08-16
+                      </p>
+                    </td>
+                    <td>4,000</td>
+                    <td>
+                      <button
+                        type="button"
+                        class="btn btn-primary-soft text-primary"
+                        data-toggle="modal"
+                        data-target="#orderCareRecord"
+                      >
+                        照護紀錄
+                      </button>
+                    </td>
+                    <td>
+                      <button
+                        type="button"
+                        class="btn btn-primary-soft text-primary"
+                        data-toggle="modal"
+                        data-target="#orderDetail"
+                      >
+                        訂單細節
+                      </button>
+                    </td>
+                    <td>
+                      <button
+                        type="button"
+                        class="btn btn-primary-soft text-primary"
+                      >
+                        終止契約
+                      </button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <div
+            class="tab-pane fade"
+            id="orderEvaluate"
+            role="tabpanel"
+            aria-labelledby="nav-contact-tab"
+          >
+            <table class="table table-radius">
+              <tr class="table-light">
+                <th scope="col">被服務對象</th>
+                <th scope="col">照服人員</th>
+                <th scope="col">服務時段</th>
+                <th scope="col">訂單金額</th>
+                <th scope="col">查看照護紀錄</th>
+                <th scope="col">查看訂單詳細</th>
+                <th scope="col">填寫評價</th>
+              </tr>
+
+              <tbody>
+                <tr>
+                  <td class="text-center">王伯伯</td>
+                  <td>張照服</td>
+                  <td>
+                    <p>
+                      2020-08-15 <br />
+                      2020-08-16
+                    </p>
+                  </td>
+                  <td>4,000</td>
+                  <td>
+                    <button
+                      type="button"
+                      class="btn btn-primary-soft text-primary"
+                      data-toggle="modal"
+                      data-target="#orderCareRecord"
+                    >
+                      照護紀錄
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      type="button"
+                      class="btn btn-primary-soft text-primary"
+                      data-toggle="modal"
+                      data-target="#orderDetail"
+                    >
+                      訂單細節
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      type="button"
+                      class="btn btn-primary"
+                      data-toggle="modal"
+                      data-target="#evaluation"
+                    >
+                      填寫評價
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td class="text-center">王伯伯</td>
+                  <td>張照服</td>
+                  <td>
+                    <p>
+                      2020-08-15 <br />
+                      2020-08-16
+                    </p>
+                  </td>
+                  <td>4,000</td>
+                  <td>
+                    <button
+                      type="button"
+                      class="btn btn-primary-soft text-primary"
+                      data-toggle="modal"
+                      data-target="#orderCareRecord"
+                    >
+                      照護紀錄
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      type="button"
+                      class="btn btn-primary-soft text-primary"
+                      data-toggle="modal"
+                      data-target="#orderDetail"
+                    >
+                      訂單細節
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      type="button"
+                      class="btn btn-primary"
+                      data-toggle="modal"
+                      data-target="#evaluation"
+                    >
+                      填寫評價
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div
+            class="tab-pane fade"
+            id="orderFinish"
+            role="tabpanel"
+            aria-labelledby="nav-contact-tab"
+          >
+            <table class="table table-radius">
+              <tr class="table-light">
+                <th scope="col">被服務對象</th>
+                <th scope="col">照服人員</th>
+                <th scope="col">服務時段</th>
+                <th scope="col">訂單金額</th>
+                <th scope="col">訂單狀態</th>
+                <th scope="col">查看照護紀錄</th>
+                <th scope="col">查看訂單詳細</th>
+              </tr>
+
+              <tbody>
+                <tr>
+                  <td class="text-center">王伯伯</td>
+                  <td>張照服</td>
+                  <td>
+                    <p>
+                      2020-08-15 <br />
+                      2020-08-16
+                    </p>
+                  </td>
+                  <td>4,000</td>
+                  <td>已完成</td>
+                  <td>
+                    <button
+                      type="button"
+                      class="btn btn-primary-soft text-primary"
+                      data-toggle="modal"
+                      data-target="#orderCareRecord"
+                    >
+                      照護紀錄
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      type="button"
+                      class="btn btn-primary-soft text-primary"
+                      data-toggle="modal"
+                      data-target="#orderDetail"
+                    >
+                      訂單細節
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td class="text-center">王伯伯</td>
+                  <td>張照服</td>
+                  <td>
+                    <p>
+                      2020-08-15 <br />
+                      2020-08-16
+                    </p>
+                  </td>
+                  <td>4,000</td>
+                  <td>已取消</td>
+                  <td>
+                    <button
+                      type="button"
+                      class="btn btn-primary-soft text-primary"
+                      data-toggle="modal"
+                      data-target="#orderCareRecord"
+                    >
+                      照護紀錄
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      type="button"
+                      class="btn btn-primary-soft text-primary"
+                      data-toggle="modal"
+                      data-target="#orderDetail"
+                    >
+                      訂單細節
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td class="text-center">王伯伯</td>
+                  <td>張照服</td>
+                  <td>
+                    <p>
+                      2020-08-15 <br />
+                      2020-08-16
+                    </p>
+                  </td>
+                  <td>4,000</td>
+                  <td>照服員拒收訂單</td>
+                  <td>
+                    <button
+                      type="button"
+                      class="btn btn-primary-soft text-primary"
+                      data-toggle="modal"
+                      data-target="#orderCareRecord"
+                    >
+                      照護紀錄
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      type="button"
+                      class="btn btn-primary-soft text-primary"
+                      data-toggle="modal"
+                      data-target="#orderDetail"
+                    >
+                      訂單細節
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
