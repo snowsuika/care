b2926f781b4ae61dc0960ea687f30b53b3b8ee8e
snow <snowsuika@gmail.com>
2020-09-05 21:38:49 +0800

    <modify> 調整家屬管理病患 UI

--- src/views/backendMember/MemberDashboardFamily.vue
+++ src/views/backendMember/MemberDashboardFamily.vue
@@ -15,28 +15,28 @@
     <div class="table-responsive">
       <table class="table table-radius">
         <tr class="table-light">
-          <th class="text-center">被服務對象</th>
-          <th>需服務項目</th>
-          <th>身體狀況</th>
-          <th class="text-center">編輯</th>
-          <th class="text-center">刪除</th>
+          <th class="text-center text-nowrap">被服務對象</th>
+          <th class="text-nowrap">需服務項目</th>
+          <th class="text-nowrap">身體狀況</th>
+          <th class="text-center text-nowrap">編輯</th>
+          <th class="text-center text-nowrap">刪除</th>
         </tr>
         <tbody>
           <tr>
-            <td class="text-center">王伯伯</td>
-            <td>
+            <td class="text-center text-nowrap">王伯伯</td>
+            <td class="text-nowrap">
               <ul class="list-unstyled">
                 <li>協助如廁</li>
                 <li>身心靈陪伴及安全維護</li>
               </ul>
             </td>
-            <td>無法行動</td>
-            <td class="text-center">
+            <td class="text-nowrap">無法行動</td>
+            <td class="text-center text-nowrap">
               <button class="btn btn-primary-soft text-primary">
                 <i class="fas fa-pen"></i> 編輯
               </button>
             </td>
-            <td class="text-center">
+            <td class="text-center text-nowrap">
               <button class="btn btn-primary-soft text-primary">
                 <i class="fas fa-trash-alt"></i> 刪除
               </button>
