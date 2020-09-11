1e7b7271251d6a45cd3a20470403c1ba8ab9a8ad
snow <snowsuika@gmail.com>
2020-09-11 11:32:08 +0800

    <modify> 修正搜尋照服員服務項目中文名稱對照

--- src/components/member/ProcessingTable.vue
+++ src/components/member/ProcessingTable.vue
@@ -20,14 +20,14 @@
         id=""
         class="btn btn-primary"
         type="hidden"
-        value="ef63e0f893ccd9b7e6a88ed2cdf7b6d8e6692d8bff2883ff0d2e96b9f7893a2ed514532f99bc12729632aa21d9061bd70b3756c6d2d364dcf53fdd699bd01dc9ca8a784efbd30795113f8d39896a0640740d0d8e5eca3da6f4fbdcd6959f1590443b15647ce6af947ad341583c2c8e9bf0ec2667ba6a49504a6cfd656a6fd38f6363deb136cf449f578ff4b62872cb593214ab1433fcc94e79bb0b70dc2bc75c71dc63d6e8222870abc0d6d1115fd7205c4c6c2e49c6d8d2edca1262cd2bb83ba9e68970911ed9d82daa74c8a0869a6be69b71dec6de3cd1b6529518023ae913ac59d213077c80c1420039bf2494483b0f7e09907d63a1f2e390cb9ba6c9cbb11bd8ecd2c22ab12de5a981c73eb2f25aa28e5b5181a08b2bb6859653675ff69dd8f1907680305d6320dc02d28596dfe96892f9a69d4ea9b2577425e209bb81f26fb5887e96717deb9561c767101e7b8049cf6857fe00e568476b4bdf005cab54"
+        value="ef63e0f893ccd9b7e6a88ed2cdf7b6d8e6692d8bff2883ff0d2e96b9f7893a2ed514532f99bc12729632aa21d9061bd738f889d6c94d8016f6cfb19607e7a350d2c724d4768c8132516e9cb4c9aebee1c28b5a9a53292961654154c6552033345ab0cec48ee8b7adcb681bf1a9149090b3056164fd2ffd6d715e061eb9af9e9e24dbee9c9c4ab004a17601a2eb78d689366068b6f4df2b2dc5ea6e671b135bd543165d30fcefb30691239d6998dddaf5a8e7cb7b2deaf8cbe455d672a5b890a45749772f57f01023a03307951134dd9d861336cae474fcbefdb6db1ea66ccb1257aab53c735a6b0243670a196632cf456a0ae5226f8ce41474c9d3abac3eca5a346e1d5097f8c4bcde58e7b1369a31b976e74b2cd7668c79f01b9321b3934f77b132bac6833f1f2029314f0fa2d2244bd0398cfd985719f891769a5bd7059891aebf246d0288fde33b1c11d75a460a172babdf43da595eee5b5ec0103feb9b0f"
       />
       <input
         name="TradeSha"
         id=""
         class="btn btn-primary"
         type="hidden"
-        value="790737665C219FC407E4C5619005EC919288B8938B7C1D6CF0400CA3D8DEAF4B"
+        value="5FD46CABA02C9E56A1F952598FAEA906C3575D96AD570E4BDC84D8EB844F7531"
       />
       <input
         name="Version"
@@ -122,7 +122,7 @@
 </template>
 
 <script>
-/* global $ */
+// /* global $ */
 export default {
   data() {
     return {
@@ -156,44 +156,44 @@ export default {
         })
         .then(res => {
           console.log(res);
-          vm.resData = res.data;
+          // vm.resData = res.data;
           // console.log(vm.$refs.subutn);
-          $('#forr').trigger('submit');
+          // $('#forr').trigger('submit');
           // $('#ss').trigger('click');
         })
         .catch(err => {
           console.log(err);
         });
-    },
-
-    cancelOrder(orderId) {
-      const vm = this;
-      vm.isLoading = true;
-      const api = `${process.env.VUE_APP_APIPATH}CancelOrder?Id=${orderId}`; //參數是訂單Id
-      vm.$http
-        .patch(api)
-        .then(res => {
-          console.log(res);
-
-          vm.$swal({
-            toast: true,
-            position: 'top-end',
-            showConfirmButton: false,
-            timer: 3000,
-            timerProgressBar: false,
-            onOpen: toast => {
-              toast.addEventListener('mouseenter', vm.$swal.stopTimer);
-              toast.addEventListener('mouseleave', vm.$swal.resumeTimer);
-            },
-            icon: 'success',
-            title: `${res.data.result}`
-          });
-          vm.isLoading = false;
-        })
-        .catch(err => {
-          console.log(err);
-        });
     }
+
+    // cancelOrder(orderId) {
+    //   const vm = this;
+    //   vm.isLoading = true;
+    //   const api = `${process.env.VUE_APP_APIPATH}CancelOrder?Id=${orderId}`; //參數是訂單Id
+    //   vm.$http
+    //     .patch(api)
+    //     .then(res => {
+    //       console.log(res);
+
+    //       vm.$swal({
+    //         toast: true,
+    //         position: 'top-end',
+    //         showConfirmButton: false,
+    //         timer: 3000,
+    //         timerProgressBar: false,
+    //         onOpen: toast => {
+    //           toast.addEventListener('mouseenter', vm.$swal.stopTimer);
+    //           toast.addEventListener('mouseleave', vm.$swal.resumeTimer);
+    //         },
+    //         icon: 'success',
+    //         title: `${res.data.result}`
+    //       });
+    //       vm.isLoading = false;
+    //     })
+    //     .catch(err => {
+    //       console.log(err);
+    //     });
+    // }
     // pay() {
     //   var formData = new FormData();
     //   formData.append('MerchantID', 'MS113893343');
